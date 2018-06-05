81cfeb18a11bf96861e97b9a9f6b4d6b3282076a
Ayoub Khial <ayouub.khial@gmail.com>
2018-06-05 04:28:19 +0100

    Adding menu and main style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,52 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
+
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    @ViewChild(Nav) nav: Nav;
+    auth: boolean;
+    rootPage: any = HomePage;
+    activePage: any;
+    pages: Array<{ title: string, component?: any, icon: string, fontAwesome: boolean }>;
+    userInformations;
+
+    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+        this.initializeApp();
+        this.pages = [
+            { title: 'Home', component: HomePage, icon: 'home', fontAwesome: false },
+            { title: 'Restaurants', component: null, icon: 'pizza', fontAwesome: false },
+            { title: 'Hotels', component: null, icon: 'bed', fontAwesome: true },
+            { title: 'Blog', component: null, icon: 'logo-rss', fontAwesome: false },
+            { title: 'Nearby', component: null, icon: 'map-marker', fontAwesome: true },
+            { title: 'About', component: null, icon: 'information-circle', fontAwesome: false },
+            { title: 'Messages', component: null, icon: 'mail', fontAwesome: false },
+            { title: 'Logout', component: null, icon: 'sign-out', fontAwesome: true },
+            { title: 'Profile', component: null, icon: 'contact', fontAwesome: false },
+            { title: 'Favorites', component: null, icon: 'heart', fontAwesome: false },
+            { title: 'Login', component: null, icon: 'sign-in', fontAwesome: true }
+        ];
+        this.activePage = this.pages[0];
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
+
+    openPage(page) {
+        this.nav.setRoot(page.component);
+        this.activePage = page;
+    }
+
+    checkActive(page) {
+        return page == this.activePage
+    }
 }
-
